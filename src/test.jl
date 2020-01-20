5672444f5f290bd27f81ba5a040a49c71751e56d
Rob Bontekoe <rbontekoe@appligate.nl>
2020-01-20 10:50:15 +0100

    Update

--- src/test.jl
+++ src/test.jl
@@ -1,25 +1,36 @@
 # test.jl
 
+const PATH_DB = "./invoicing.sqlite"
+
+const PATH_CSV = "./bank.csv"
+
 # get orders
 using AppliSales
 
 orders = AppliSales.process()
 
-# create unpaid invoices from order and journal entries for generalledger
+# process orders
 using AppliInvoicing
 
-journal_entries_unpaid_invoices = AppliInvoicing.process(orders)
+journal_entries_1 = AppliInvoicing.process(PATH_DB, orders)
+
+# get Bank statemnets and unpaid invoices
+stms = AppliInvoicing.read_bank_statements(PATH_CSV)
+
+unpaid_invoices = AppliInvoicing.retrieve_unpaid_invoices(PATH_DB)
+
+# process unpaid invoices and bank staements
+journal_entries_2 = AppliInvoicing.process(PATH_DB, unpaid_invoices, stms)
+
+# =============================
 
 # process journal entries
 using AppliGeneralLedger
 
-AppliGeneralLedger.process(journal_entries_unpaid_invoices)
-
-# process bankstatements
-journal_entries_invoices_paid = AppliInvoicing.process()
+AppliGeneralLedger.process(journal_entries_1)
 
 # process journal entries
-AppliGeneralLedger.process(journal_entries_invoices_paid)
+AppliGeneralLedger.process(journal_entries_2)
 
 # get data form general ledger
 using AppliSQLite
@@ -37,16 +48,16 @@ r = retrieve(db, "LEDGER", "accountid = 1300")
 
 account_receivable = sum(r.debit - r.credit)
 
-println("Status accounts receivable: € $account_receivable")
+println("Status accounts receivable: € $account_receivable") # should be € 1210.0
 
 # get status of sales
 r = retrieve(db, "LEDGER", "accountid = 8000")
 
-sales = sum(r.credit - r.debit)
+sales = sum(r.credit - r.debit) # should retirn € 4000.0
 
 println("Sales: € $sales")
 
 # cleanup
-stm = `rm invoicing.sqlite ledger.sqlite`
+#stm = `rm invoicing.sqlite ledger.sqlite`
 
-run(stm)
+#run(stm)

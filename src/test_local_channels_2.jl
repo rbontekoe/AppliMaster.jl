7987bb22cf9f712f245a8cc7970d33ba5971735b
Rob Bontekoe <rbontekoe@appligate.nl>
2020-05-07 09:40:36 +0200

    Update comments in test_local_channels2.jl

--- src/test_local_channels_2.jl
+++ src/test_local_channels_2.jl
@@ -13,7 +13,6 @@ using Distributed
 
 # this should be the next step
 np = addprocs(4; exeflags=`--project=$(Base.active_project())`)
-#np = addprocs([("rob@192.168.2.77:2222", :auto)]; exeflags=`--project=$(Base.active_project())`)
 @info("number of processes is $(length(np))")
 
 # activate the packages (before the processes are created)
@@ -32,10 +31,10 @@ include("./api/api.jl")
 rx = dispatcher()
 @info("Dispatcher started")
 
-# start remote
+# start application remote
 include("client.jl")
 
-# aging report
+# display aging report
 using DataFrames
 
 sleep(10)
